994c72654761bdb9a9be6531227e3a3079b81261
Legaz <worldoflegaz@icloud.com>
2025-07-27 19:44:26 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Comment from './components/Comment'
+
+function App() {
+  return (
+    <div className='bg-Grey-50'>
+    <Comment />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Comment.tsx
@@ -0,0 +1,21 @@
+import "../context/data.json";
+
+const Comment = () => {
+  return (
+    <div className="container mx-auto bg-White">
+        <div>
+            <div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-5">
+                  <img src="/images/avatars/image-amyrobson.png" alt="avatar-img"  className="h-10 w-10"/> 
+                  <p>1 month ago</p>
+                  </div>
+                  <button className="flex  items-center cursor-pointer gap-5"><img src="/images/icon-reply.svg" alt="reply-icon"  className="object-contain"/> Reply</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Comment
