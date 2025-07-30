48b24297dd5826d70934229384e52e7d364a3bd5
Legaz <worldoflegaz@icloud.com>
2025-07-31 00:43:26 +0900

    maping data

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,21 +1,35 @@
-import "../context/data.json";
+import data from "../context/data.json";
 
 const Comment = () => {
   return (
     <div className="container mx-auto bg-White">
-        <div>
-            <div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center gap-5">
-                  <img src="/images/avatars/image-amyrobson.png" alt="avatar-img"  className="h-10 w-10"/> 
-                  <p></p>
-                  </div>
-                  <button className="flex  items-center cursor-pointer gap-5"><img src="/images/icon-reply.svg" alt="reply-icon"  className="object-contain"/> Reply</button>
-                </div>
+      <div>
+        {data.comments.map((comment) => (
+          <div
+            key={comment.id}
+            className="flex items-center justify-between my-4"
+          >
+            <div className="flex items-center gap-5">
+              <img
+                src={comment.user.image.png}
+                alt="avatar-img"
+                className="h-10 w-10"
+              />
+              <p className="font-semibold">{comment.user.username}</p>
             </div>
-        </div>
+            <button className="flex items-center cursor-pointer gap-2 text-Blue">
+              <img
+                src="/images/icon-reply.svg"
+                alt="reply-icon"
+                className="object-contain"
+              />
+              Reply
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;
