39a56336a32a0f2370c6f9df39e51a034e706aa7
Legaz <worldoflegaz@icloud.com>
2025-07-31 02:52:50 +0900

    added

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,31 +3,46 @@ import data from "../context/data.json";
 const Comment = () => {
   return (
     <div className="container mx-auto bg-White">
-      <div>
+      <>
         {data.comments.map((comment) => (
-          <div
-            key={comment.id}
-            className="flex items-center justify-between my-4"
-          >
-            <div className="flex items-center gap-5">
-              <img
-                src={comment.user.image.png}
-                alt="avatar-img"
-                className="h-10 w-10"
-              />
-              <p className="font-semibold">{comment.user.username}</p>
+          <div className="flex p-5 gap-3">
+            <div className="bg-Grey-50 p-3 rounded-md gap-2 justify-between flex flex-col">
+              <img src="/images/icon-plus.svg" alt="plus" />
+              {comment.score}
+              <img src="/images/icon-minus.svg" alt="minus" />
             </div>
-            <button className="flex items-center cursor-pointer gap-2 text-Blue">
-              <img
-                src="/images/icon-reply.svg"
-                alt="reply-icon"
-                className="object-contain"
-              />
-              Reply
-            </button>
+            <div>
+              <div
+                key={comment.id}
+                className="flex items-center justify-between my-"
+              >
+                <div className="flex items-center gap-5">
+                  <div className="flex items-center gap-5">
+                    <img
+                      src={comment.user.image.png}
+                      alt="avatar-img"
+                      className="h-10 w-10"
+                    />
+                    <p className="font-semibold">{comment.user.username}</p>
+                  </div>
+                  <p>{comment.createdAt}</p>
+                </div>
+                <button className="flex items-center cursor-pointer gap-2 text-Blue">
+                  <img
+                    src="/images/icon-reply.svg"
+                    alt="reply-icon"
+                    className="object-contain"
+                  />
+                  Reply
+                </button>
+              </div>
+              <div className="">
+                <p>{comment.content}</p>
+              </div>
+            </div>
           </div>
         ))}
-      </div>
+      </>
     </div>
   );
 };
